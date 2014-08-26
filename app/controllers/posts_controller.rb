683c16abe3bf7d56e7af770652ac92b1bb176490
dobestan <dobestan@gmail.com>
2014-08-26 11:30:17 +0900

    NEW/CREATE action/view 생성하기

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  # NEW -> CREATE
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+
+    # post_path(post), 즉 새로 생성된 post의 show action으로 이동한다.
+    redirect_to post_path(post)
+  end
+
+  private
+    def post_params
+      # post라는 이름의 form 에 대해서 ( form_for @post / form_for :post )
+      # title, content라는 항목에 대해서만 parameter로 받는다
+      params.require(:post).permit(:title, :content)
+    end
 end
