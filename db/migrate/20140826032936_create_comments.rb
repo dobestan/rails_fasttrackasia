072a4035269a0ac75e07584bc8fadadc3250a7ff
dobestan <dobestan@gmail.com>
2014-08-26 12:34:05 +0900

    Comment Model 정의하기 > t.references에 유의하기

--- db/migrate/20140826032936_create_comments.rb
+++ db/migrate/20140826032936_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :commenter
+      t.text :content
+      t.references :post
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826021041) do
+ActiveRecord::Schema.define(version: 20140826032936) do
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "content"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
