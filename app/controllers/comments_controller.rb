c274e1453faa64286182a7afa7afafcdc9630c00
dobestan <dobestan@gmail.com>
2014-08-28 18:30:01 +0900

    Comment > DESTROY and update show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(post)
   end
 
+  def destroy
+    post = Post.find(params[:post_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to post_path(post)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :content)
